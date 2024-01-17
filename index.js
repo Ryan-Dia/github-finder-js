6c003570691dc915348271255c49d8d77a87a205
ryan-dia <cjfdnjs158@naver.com>
2024-01-17 21:34:31 +0900

    feat: 사용자 데이터를 html 렌더링 하는 기능 추가

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import fetchUserData from './data.js';
+import {
+  renderProfile,
+  renderUserFollowers,
+  renderUserRepositories,
+  userNotFound,
+} from './render.js';
 
 const searchInput = document.querySelector('.search__input');
 const searchButton = document.querySelector('.search__button');
 
 const run = async (userName) => {
   const data = await fetchUserData(userName);
+
+  if (data.login === undefined) {
+    userNotFound();
+    return;
+  }
+
+  renderProfile(data);
+  renderUserRepositories(userName);
+  renderUserFollowers(userName);
 };
 
 // Event Listeners
